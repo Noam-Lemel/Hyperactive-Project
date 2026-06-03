ec410d91691922a34789f8202cc9a64ee4b535ca
Noam Lemel <noam123435@gmail.com>
2026-06-03 15:09:58 +0300

    Initial commit - Hyperactive landing page

---
+++ main.js
@@ -0,0 +1,71 @@
+const humburgerBtn=document.getElementById('humburger-container');
+const headerNav=document.getElementById('link-container');
+const careerItem=document.querySelector('.item');
+const careerLink=document.getElementById('career-link');
+const mobileCareer=document.getElementById('mobile-career');
+const body=document.body;
+const circleArrows= document.getElementsByClassName('circle-arrow');
+const qaContainers=document.getElementsByClassName('qa-container');
+const exitButtons=document.querySelectorAll('.exit-button');
+const forms=document.getElementsByTagName('form');
+const modalsbackground=document.getElementById('modals-background');
+const confirmationModal=document.getElementById('confirmation-modal');
+const formModal=document.getElementById('form-modal');
+const pluses=document.querySelectorAll('.plus');
+const boxes=document.querySelectorAll('.box');
+const privacyBtns=document.querySelectorAll('.privacy-btn');
+const accessibilityBtns=document.querySelectorAll('.accessibility-btn');
+const privecyModal=document.querySelector('.privacy-modal');
+const accessibilityModal=document.querySelector('.accessibility-modal');
+
+humburgerBtn.addEventListener('click',()=>{
+        headerNav.classList.toggle('isOpen');
+        humburgerBtn.classList.toggle('isOpen');
+});
+for(let i=0;i<qaContainers.length;i++){
+    qaContainers[i].addEventListener('click',()=>{
+        qaContainers[i].classList.toggle('isOpen');
+    })
+}
+for(let i=0;i<boxes.length;i++){
+    boxes[i].addEventListener('click',()=>{
+        boxes[i].classList.toggle('isOpen');
+    })
+}
+for(let i=0;i<exitButtons.length;i++){
+    exitButtons[i].addEventListener('click',()=>{
+        const modal=exitButtons[i].parentElement;
+        modal.classList.add('none');
+         modalsbackground.classList.add('none');
+    });
+}
+for(let i=0;i<forms.length;i++){
+    forms[i].addEventListener('submit',(event)=>{
+        event.preventDefault();
+        if(!formModal.classList.contains('none'))
+            formModal.classList.add('none');
+        modalsbackground.classList.remove('none');
+        confirmationModal.classList.remove('none');
+    });
+}
+careerItem.addEventListener('click',(event)=>{
+    if(window.innerWidth<1000)
+        event.preventDefault();
+        careerItem.classList.toggle('isDropdownOpen');
+});
+humburgerBtn.addEventListener('click',()=>{
+    headernav.classList.toggle('isOpen');
+    humburgerBtn.classList.toggle('isOpen');
+})
+for(let i=0;i<privacyBtns.length;i++){
+    privacyBtns[i].addEventListener('click',()=>{
+        privecyModal.classList.remove('none');
+        modalsbackground.classList.remove('none');
+    })
+}
+for(let i=0;i<accessibilityBtns.length;i++){
+    accessibilityBtns[i].addEventListener('click',()=>{
+        accessibilityModal.classList.remove('none');
+        modalsbackground.classList.remove('none');
+    })
+}
